381c123498c286d8393b033f4a924b681b59e3f4
OtterWhisperer <sundowneffect@gmail.com>
2020-12-13 02:34:19 +0100

    Add error handling and small optimizations

--- src/scuffed_fuel_estimator.py
+++ src/scuffed_fuel_estimator.py
@@ -13,6 +13,11 @@ def inversedshorten(s, subs):
     i = s.index(subs)
     return s[i + len(subs):]
 
+def validateFloatInput(value):
+    try:
+        return float(value)
+    except ValueError:
+        print("Wrong value!")
 
 #The car object (maybe not the right name for that? yes).
 class Car:
@@ -21,6 +26,11 @@ class Car:
         self.carburant_prices = []
         self.carburant_names = []
         self.carburant_types = []
+        self.get_page_info()
+        self.extract_html_names()
+        self.extract_html_prices()
+        self.prices_to_float()
+        self.get_car_info()
 
     #Request page in HTML and extract names + prices and merge them into carburant_types list (with the shorten function I made before).
     def get_page_info(self):
@@ -81,84 +91,91 @@ class Car:
     
     #Getting user inputs for variables.
     def get_car_info(self):
-        self.type = input("What type of fuel do you use? (list below, type the letter)\nSans Plomb 95 E5 (a)\nSans Plomb 95 E10 (b)\nBioethanol E85 (c)\nGazole B7 (d)\nGPL (e)\n").lower()
-        self.refuel = float(input("Last refuel total amount (in L): "))
-        self.fuelamount = float(input("Current fuel amount (in L): "))
-        self.rerefuel = float(input("How much fuel do you plan on for your next refuel ? (in L): "))
+        self.type = input("What type of fuel do you use? (list below, type the letter)\nSans Plomb 95 E5 (a)\nSans Plomb 95 E10 (b)\nBioethanol E85 (c)\nGazole B7 (d)\nGPL (e)\nExit the program (exit)\n").lower()
+        while self.type not in {"a", "b", "c", "d", "e", "exit"}:
+            print("Only values of a, b, c, d or e are allowed!")
+            self.type = input("What type of fuel do you use? (list below, type the letter)\nSans Plomb 95 E5 (a)\nSans Plomb 95 E10 (b)\nBioethanol E85 (c)\nGazole B7 (d)\nGPL (e)\nExit the program (exit)\n").lower()
+        if (self.type == "exit"): 
+            exit("See you soon!")
+        self.refuel = input("Last refuel total amount (in L): ")
+        while not self.refuel.isnumeric():
+            print("Only numeric values are allowed!")
+            self.refuel = input("Last refuel total amount (in L): ")
+        self.refuel = validateFloatInput(self.refuel)
+
+        self.fuelamount = input("Current fuel amount (in L): ")
+        while not self.fuelamount.isnumeric():
+            print("Only numeric values are allowed!")
+            self.fuelamount = input("Current fuel amount (in L): ")
+        self.fuelamount = validateFloatInput(self.fuelamount)
+
+        self.rerefuel = input("How much fuel do you plan on for your next refuel ? (in L): ")
+        while not self.rerefuel.isnumeric():
+            print("Only numeric values are allowed!")
+            self.rerefuel = input("How much fuel do you plan on for your next refuel ? (in L): ")
+        self.rerefuel = validateFloatInput(self.rerefuel)
+
+        print(self.moneyspent())
+        print(self.equivalentmoneyfuel())
+        print(self.equivalentmoneyrefuel())
+        self.__init__()
 
     #Fuel equivalent in money.
     def moneyspent(self):
         if self.type == 'a':
             total = self.sansplomb95e5_price * self.refuel
-            return f"Your last refuel ({self.refuel} L) cost {total} €"
-
+    
         elif self.type == 'b':
             total = self.sansplomb95e10_price * self.refuel
-            return f"Your last refuel ({self.refuel} L) cost {total} €"
-
+    
         elif self.type == 'c':
             total = self.bioethanole85_price * self.refuel
-            return f"Your last refuel ({self.refuel} L) cost {total} €"
-
+    
         elif self.type == 'd':
             total = self.gazoleb7_price * self.refuel
-            return f"Your last refuel ({self.refuel} L) cost {total} €"
-
+        
         elif self.type == 'e':
             total = self.gpl_price * self.refuel
-            return f"Your last refuel ({self.refuel} L) cost {total} €"
+        
+        return f"Your last refuel ({self.refuel} L) cost {total} €"
 
     #Lasting fuel equivalent in money.
     def equivalentmoneyfuel(self):
         if self.type == 'a':
             lasting = self.sansplomb95e5_price * self.fuelamount
-            return f"With {self.fuelamount} L lasting, you have the equivalent of {lasting} € to drive before another refuel."
 
         elif self.type == 'b':
             lasting = self.sansplomb95e10_price * self.fuelamount
-            return f"With {self.fuelamount} L lasting, you have the equivalent of {lasting} € to drive before another refuel."
 
         elif self.type == 'c':
             lasting = self.bioethanole85_price * self.fuelamount
-            return f"With {self.fuelamount} L lasting, you have the equivalent of {lasting} € to drive before another refuel."
 
         elif self.type == 'd':
             lasting = self.gazoleb7_price * self.fuelamount
-            return f"With {self.fuelamount} L lasting, you have the equivalent of {lasting} € to drive before another refuel."
 
         elif self.type == 'e':
             lasting = self.gpl_price * self.fuelamount
-            return f"With {self.fuelamount} L lasting, you have the equivalent of {lasting} € to drive before another refuel."
+
+        return f"With {self.fuelamount} L lasting, you have the equivalent of {lasting} € to drive before another refuel."
 
     #Reuel equivalent in money.
     def equivalentmoneyrefuel(self):
         if self.type == 'a':
             total1 = self.sansplomb95e5_price * self.rerefuel
-            return f"Your next refuel (you plan {self.rerefuel} L) will cost {total1} €"
 
         elif self.type == 'b':
             total1 = self.sansplomb95e10_price * self.rerefuel
-            return f"Your next refuel (you plan {self.rerefuel} L) will cost {total1} €"
 
         elif self.type == 'c':
             total1 = self.bioethanole85_price * self.rerefuel
-            return f"Your next refuel (you plan {self.rerefuel} L) will cost {total1} €"
 
         elif self.type == 'd':
             total1 = self.gazoleb7_price * self.rerefuel
-            return f"Your next refuel (you plan {self.rerefuel} L) will cost {total1} €"
 
         elif self.type == 'e':
             total1 = self.gpl_price * self.rerefuel
-            return f"Your next refuel (you plan {self.rerefuel} L) will cost {total1} €"
+
+        return f"Your next refuel (you plan {self.rerefuel} L) will cost {total1} €"
 
 #Call object and its functions.
 car = Car()
-car.get_page_info()
-car.extract_html_names()
-car.extract_html_prices()
-car.prices_to_float()
-car.get_car_info()
-print(car.moneyspent())
-print(car.equivalentmoneyfuel())
-print(car.equivalentmoneyrefuel())
